dcb35a5f884a37ec3a5c4fbe52b2a6a2b4b5889e
UTKARSH RAZ <utkarshraj525@gmail.com>
2025-10-17 15:19:12 +0530

    Generate A home Page UI

--- CRMS/crms_client/src/UI/logo/RefTrackLogo.jsx
+++ CRMS/crms_client/src/UI/logo/RefTrackLogo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const RefTrackLogo = () => {
   return (
     <Link
-      to='/dashboard'
+      to='/'
       className='flex items-center gap-3 text-xl sm:text-2xl font-semibold text-gray-800'
     >
       <div className='bg-primary p-2 rounded-lg shadow-md flex items-center justify-center'>
