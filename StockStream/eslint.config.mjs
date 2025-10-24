10c90a40b942d284043757966bb4917b3ea2d83b
UTKARSH RAZ <utkarshraj525@gmail.com>
2025-10-24 09:55:01 +0530

    Initial deploy setup

---
+++ StockStream/eslint.config.mjs
@@ -0,0 +1,17 @@
+"use strict";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+export default tseslint.config({
+    ignores: ['dist']
+}, {
+    files: ['**/*.ts'],
+    languageOptions: {
+        globals: globals.node
+    },
+    extends: [pluginJs.configs.recommended, ...tseslint.configs.recommended],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'no-console': 'off'
+    }
+});
